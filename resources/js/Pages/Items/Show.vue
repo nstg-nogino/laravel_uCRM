e9ac7737e4579d9f3c1ab93407f6e6a9670cc645
nstg-nogino <nogino@nstg.co.jp>
2023-06-09 14:38:53 +0900

    customerSearch

--- resources/js/Pages/Items/Show.vue
+++ resources/js/Pages/Items/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/inertia-vue3';
 import { Inertia } from '@inertiajs/inertia'
 import { nl2br } from '@/comon';
 
