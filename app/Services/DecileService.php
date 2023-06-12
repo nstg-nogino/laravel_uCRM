f11aceef712afd2969ac1b7fcff07addc8e0bd7a
nstg-nogino <nogino@nstg.co.jp>
2023-06-12 17:07:33 +0900

    decile

--- app/Services/DecileService.php
+++ app/Services/DecileService.php
@@ -1,54 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Services;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Order;
 use Illuminate\Support\Facades\DB; 
 
-class AnalysisController extends Controller
-{
-    public function index(){
-
-        $startDate ='2022-08-01';
-        $endDate = '2022-08-10';
-
-        // $period = Order::betweenDate($startDate, $endDate)
-        // ->groupBy('id')
-        // ->selectRaw('id, customer_name, sum(subtotal) as total, status, created_at' )
-        // ->orderBy('created_at')
-        // ->paginate(50); 
-
-        // dd($period);
-
-        // $subQuery = Order::betweenDate($startDate, $endDate)
-        // ->where('status', true)
-        // ->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as totalPerPurchase, 
-        //     DATE_FORMAT(created_at, "%Y%m%d") as date' );
-            
-        // $data = DB::table($subQuery)
-        // ->groupBy('date')
-        // ->selectRaw('date, sum(totalPerPurchase) as total')
-        // ->get();
-
-        // dd($data);      
-
-
-        return Inertia::render('Analysis');
-        
-    }
-
-    public function decilLog(){
-
-        $startDate ='2022-08-01';
-        $endDate = '2022-08-10';
-
+class DecileService {
+    public static function decile($subQuery){
         // 1. 購買ID毎にまとめる
-        $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
+        $subQuery = $subQuery->groupBy('id')
         ->selectRaw('id, customer_id, customer_name, 
             SUM(subtotal) as totalPerPurchase'); 
 
@@ -122,8 +81,10 @@ class AnalysisController extends Controller
         totalRatio
         ')
         ->get();
-        
 
-        // dd($data);
+        $labels = $data->pluck('decile');
+        $totals = $data->pluck('totalPerGroup');
+
+        return [$data, $labels, $totals];
     }
 }
